2eabdfcbb903b039d31e02d96c9be670192153eb
wasim-redalkemi <wasim.redalkemi@gmail.com>
2022-08-23 17:25:57 +0530

    changes

--- server.js
+++ server.js
@@ -3,9 +3,23 @@ const request = require('request');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
 app.get("/", (req, res) =>{
-  res.redirect("https://themoviesking.herokuapp.com");
+  res.redirect(301, "https://themoviesking.herokuapp.com");
 });
+app.get("/download/download-superman-and-lois-season-1-hindi-720p-1080p", (req, res) =>{
+  res.redirect(301, "https://themoviesking.herokuapp.com/download/download-superman-and-lois-season-1-hindi-720p-1080p");
+});
+app.get("/category/netflix", (req, res) =>{
+  res.redirect(301, "https://themoviesking.herokuapp.com/category/netflix");
+});
+app.get("/category/web-series", (req, res) =>{
+  res.redirect(301, "https://themoviesking.herokuapp.com/category/web-series");
+});
+app.get("/download/download-lekh-2022-punjabi-movie-480p-720p-1080p", (req, res) =>{
+  res.redirect(301, "https://themoviesking.herokuapp.com/download/download-lekh-2022-punjabi-movie-480p-720p-1080p");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
